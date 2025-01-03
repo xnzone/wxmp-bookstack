5cb16d2fd57c5d013bd43b1197d08b0fd3943974
xnzone <xnzone@qq.com>
2025-01-03 18:24:57 +0800

    update menu

--- 04_distributed_systems_for_fun_and_profit_zh/docs/menu.js
+++ 04_distributed_systems_for_fun_and_profit_zh/docs/menu.js
@@ -1,16 +1,1 @@
-export const menuConfig = [
-  {
-    "id": "04_distributed_systems_for_fun_and_profit_zh",
-    "title": "",
-    "children": [
-      {
-        "id": "04-distributed-systems-for-fun-and-profit-zh_00-cover",
-        "title": "趣谈分布式系统"
-      },
-      {
-        "id": "01_distributed_systems_at_a_high_leve_zh",
-        "title": "分布式系统概览"
-      }
-    ]
-  }
-]
+export const menuConfig = [{"id":"04_distributed_systems_for_fun_and_profit_zh","title":"","children":[{"id":"04-distributed-systems-for-fun-and-profit-zh_00-cover","title":"趣谈分布式系统"},{"id":"01_distributed_systems_at_a_high_leve_zh","title":"分布式系统概览"}]}]
