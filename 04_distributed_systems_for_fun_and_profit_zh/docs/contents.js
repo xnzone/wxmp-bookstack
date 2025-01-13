225cca9d45e3ea706bb86874a9f4282004dbca02
xnzone <xnzone@qq.com>
2025-01-13 10:46:33 +0800

    update distributed systems

--- 04_distributed_systems_for_fun_and_profit_zh/docs/contents.js
+++ 04_distributed_systems_for_fun_and_profit_zh/docs/contents.js
@@ -1,5 +1,6 @@
 module.exports = {
   '04-distributed-systems-for-fun-and-profit-zh_00-cover': require('./04-distributed-systems-for-fun-and-profit-zh/00-cover.js'),
   '01_distributed_systems_at_a_high_leve_zh': require('./04-distributed-systems-for-fun-and-profit-zh/01-distributed-systems-at-a-high-leve-zh.js'),
+  '01_distributed_systems_at_a_high_level_zh': require('./04-distributed-systems-for-fun-and-profit-zh/01-distributed-systems-at-a-high-level-zh.js'),
   '04_distributed_systems_for_fun_and_profit_zh': require('./04-distributed-systems-for-fun-and-profit-zh/00-cover.js'),
 };
